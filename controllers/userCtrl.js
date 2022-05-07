cc526872aa867142656a292cfc77575d1b8b8d6e
igorraknorwid <igorrakfed@gmail.com>
2022-05-07 22:20:13 +0200

    corse

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -81,7 +81,9 @@ const userCtrl = {
   },
   refreshToken: (req, res) =>{
     try {
+        
         const rf_token = req.cookies.refreshtoken;
+        console.log("rf_token",rf_token)
         if(!rf_token) return res.status(400).json({msg: "Please Login or Register"})
 
         jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
