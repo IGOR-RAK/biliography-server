24b1bd23298f15a8f3bf761334db0d4f3a992bf2
igorraknorwid <igorrakfed@gmail.com>
2022-05-08 08:50:43 +0200

    cors

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({credentials:true,origin:"http://localhost:3000"}));
-app.use(cors());
+app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+// app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
